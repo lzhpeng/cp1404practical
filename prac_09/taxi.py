ae1bace80067507c5d68db32b6cc476960f9fecb
LZP <zhipeng.li@my.jcu.edu.au>
2024-04-14 19:48:43 +0800

    8

---
+++ prac_09/taxi.py
@@ -0,0 +1,25 @@
+from prac_09.car import Car
+
+
+class Taxi(Car):
+
+    price_per_km = 1.23
+
+    def __init__(self, name, fuel):
+        super().__init__(name, fuel)
+        self.current_fare_distance = 0
+
+    def __str__(self):
+        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
+
+    def get_fare(self):
+        fare = round(self.price_per_km * self.current_fare_distance, 1)
+        return fare
+
+    def start_fare(self):
+        self.current_fare_distance = 0
+
+    def drive(self, distance):
+        distance_driven = super().drive(distance)
+        self.current_fare_distance += distance_driven
+        return distance_driven
